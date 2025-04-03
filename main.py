bbf0c637866e4d22219af2d3a0151bd51c8de89f
Lee <152331983+theTish@users.noreply.github.com>
2025-04-03 16:24:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -187,8 +187,10 @@ def start_climate():
     try:
         vehicle_manager.update_all_vehicles_with_cached_state()
 
+        print("Allowed temps:", vehicle_manager.allowed_temperatures)
+        
         climate_options = ClimateRequestOptions(
-            set_temp=68,
+            set_temp=70,
             duration=10,
             defrost=True,
             heating=True,
