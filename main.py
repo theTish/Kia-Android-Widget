5db4765a845c071eaeb34a69fddf4b7bbe1496fa
Lee <152331983+theTish@users.noreply.github.com>
2025-04-04 17:02:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ def start_climate():
         climate_options = ClimateRequestOptions(
             set_temp=20,
             defrost=False,
-            front_left_seat=3,
+            front_left_seat=0,
             duration=10
         )
 
